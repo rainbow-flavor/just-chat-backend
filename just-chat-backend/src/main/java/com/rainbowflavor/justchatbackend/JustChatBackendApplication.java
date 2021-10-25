52d3a288c354177e6205f18e23624b8099118022
irostub <irostub@gmail.com>
2021-10-25 15:26:45 +0900

    Init backend

---
+++ just-chat-backend/src/main/java/com/rainbowflavor/justchatbackend/JustChatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rainbowflavor.justchatbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustChatBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JustChatBackendApplication.class, args);
+    }
+
+}
---
+++ just-chat-backend/src/test/java/com/rainbowflavor/justchatbackend/JustChatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rainbowflavor.justchatbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustChatBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
